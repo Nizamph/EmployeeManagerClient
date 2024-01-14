9f266403c9e35f4089a2765b38b03477b5c72814
Nizamph <116118003+Nizamph@users.noreply.github.com>
2024-01-14 16:55:36 +0530

    production api added

--- src/constants/utils.js
+++ src/constants/utils.js
@@ -1,12 +1,15 @@
-export const SIGN_UP = 'http://localhost:4000/api/auth/register';
-export const LOGIN = 'http://localhost:4000/api/auth/login';
+export const SIGN_UP =
+  'https://employeemanagementserver.onrender.com/api/auth/register';
+export const LOGIN =
+  'https://employeemanagementserver.onrender.com/api/auth/login';
 export const GET_ALL_EMPLOYEES =
-  'http://localhost:4000/api/employee/employeesList';
+  'https://employeemanagementserver.onrender.com/api/employee/employeesList';
 
-export const ADD_EMPLOYEE = 'http://localhost:4000/api/employee/addEmployee';
+export const ADD_EMPLOYEE =
+  'https://employeemanagementserver.onrender.com/api/employee/addEmployee';
 export const DELETE_EMPLOYEE =
-  'http://localhost:4000/api/employee/deleteEmployee';
+  'https://employeemanagementserver.onrender.com/api/employee/deleteEmployee';
 export const EDIT_EMPLOYEE =
-  'http://localhost:4000/api/employee/updateEmployee';
+  'https://employeemanagementserver.onrender.com/api/employee/updateEmployee';
 export const PAGINATED_DATA =
-  'http://localhost:4000/api/employee/paginatedEmployees';
+  'https://employeemanagementserver.onrender.com/api/employee/paginatedEmployees';
